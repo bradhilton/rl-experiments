feat: Add weighting + fix input_pos bug

experiments/lib/grpo.py
@@ -108,6 +108,7 @@ class GRPO(torch.nn.Module):
         logprobs: torch.Tensor,
         reference_logprobs: torch.Tensor | None,
         mask: torch.Tensor,
+        weights: torch.Tensor,
         bos_id: int,
     ) -> GRPOResult:
         """
@@ -133,6 +134,9 @@ class GRPO(torch.nn.Module):
             mask (Tensor):
                 Shape: (batch_size, sequence_length)
                 Boolean mask specifying positions where loss should be computed.
+            weights (Tensor):
+                Shape: (batch_size, sequence_length)
+                Weights for each token.
             bos_id (int):
                 Index of the beginning of sequence token in the vocabulary.
 
@@ -153,12 +157,20 @@ class GRPO(torch.nn.Module):
                     else [None] * num_chunks
                 ),
                 mask.chunk(num_chunks, dim=1),
+                weights.chunk(num_chunks, dim=1),
             ):
                 result += self._forward_chunk(*chunked_args, bos_id=bos_id)
             return result
 
         return self._forward_chunk(
-            logits, tokens, advantages, logprobs, reference_logprobs, mask, bos_id
+            logits,
+            tokens,
+            advantages,
+            logprobs,
+            reference_logprobs,
+            mask,
+            weights,
+            bos_id,
         )
 
     def _forward_chunk(
@@ -169,6 +181,7 @@ class GRPO(torch.nn.Module):
         logprobs: torch.Tensor,
         reference_logprobs: torch.Tensor | None,
         mask: torch.Tensor,
+        weights: torch.Tensor,
         bos_id: int,
     ) -> GRPOResult:
         """
@@ -186,6 +199,7 @@ class GRPO(torch.nn.Module):
         if reference_logprobs is not None:
             reference_logprobs = shift_tensor(reference_logprobs, 0).view(-1)
         mask = shift_tensor(mask, False).view(-1)  # (batch_size * sequence_length,)
+        weights = shift_tensor(weights, 0).view(-1)  # (batch_size * sequence_length,)
         num_tokens = mask.sum()
         dist = torch.distributions.Categorical(logits=logits)
         entropy = dist.entropy()[mask]
@@ -213,11 +227,12 @@ class GRPO(torch.nn.Module):
             )
         else:
             kl_div = torch.tensor(torch.nan, device=logits.device)
+        weights = weights[mask]
         return GRPOResult(
             num_tokens=num_tokens,
-            policy_loss=policy_loss.sum(),
-            entropy=entropy.sum(),
-            kl_div=kl_div.sum(),
+            policy_loss=policy_loss.mul(weights).sum(),
+            entropy=entropy.mul(weights).sum(),
+            kl_div=kl_div.mul(weights).sum(),
             entropy_weight=self.entropy_coef * num_tokens,
             kl_weight=self.kl_coef * num_tokens,
         )

experiments/lib/pack.py
@@ -17,6 +17,7 @@ class PackedTensors(TypedDict):
     assistant_mask: torch.Tensor
     logprobs: torch.Tensor
     advantages: torch.Tensor
+    weights: torch.Tensor
 
 
 class DiskPackedTensors(TypedDict):
@@ -49,7 +50,7 @@ def packed_tensors_from_tokenized_results(
     assistant_mask: list[list[int]] = [[]]
     logprobs: list[list[float]] = [[]]
     advantages: list[list[float]] = [[]]
-
+    weights: list[list[float]] = [[]]
     for result in tokenized_results:
         if len(result.token_ids) > seq_len and not truncate_long_results:
             print("Result is too long, skipping")
@@ -71,6 +72,7 @@ def packed_tensors_from_tokenized_results(
             assistant_mask.append([])
             logprobs.append([])
             advantages.append([])
+            weights.append([])
         group_id = random.randint(-(2**63), 2**63 - 1)
         if result.prompt_id in group_ids[-1]:
             result = result_without_prompt
@@ -80,7 +82,7 @@ def packed_tensors_from_tokenized_results(
             + [group_id] * (len(result.token_ids) - result.prompt_length)
         )
         parent_ids[-1].extend([result.prompt_id] * len(result.token_ids))
-        input_pos[-1].extend(range(len(result.token_ids)))
+        input_pos[-1].extend(result.input_pos)
         assistant_mask[-1].extend(result.assistant_mask)
         offset = len(logprobs[-1])
         logprobs[-1].extend([float("nan")] * len(result.token_ids))
@@ -92,6 +94,7 @@ def packed_tensors_from_tokenized_results(
             for idx, token_logprob in zip(assistant_indices, result.token_logprobs):
                 logprobs[-1][idx + offset] = token_logprob.logprob
         advantages[-1].extend([result.advantage] * len(result.token_ids))
+        weights[-1].extend([1 / sum(result.assistant_mask)] * len(result.token_ids))
         if truncate_long_results:
             token_ids[-1] = token_ids[-1][:seq_len]
             group_ids[-1] = group_ids[-1][:seq_len]
@@ -99,6 +102,7 @@ def packed_tensors_from_tokenized_results(
             assistant_mask[-1] = assistant_mask[-1][:seq_len]
             logprobs[-1] = logprobs[-1][:seq_len]
             advantages[-1] = advantages[-1][:seq_len]
+            weights[-1] = weights[-1][:seq_len]
 
     def pad(values: list[list], pad_value) -> list[list]:
         max_len = seq_len
@@ -106,14 +110,24 @@ def packed_tensors_from_tokenized_results(
             value.extend([pad_value] * (max_len - len(value)))
         return values
 
+    assistant_mask_tensor = torch.tensor(pad(assistant_mask, 0), dtype=torch.bool)
+    weights_tensor = torch.tensor(pad(weights, 0.0))
+    weights_tensor = torch.where(
+        assistant_mask_tensor, weights_tensor, torch.zeros_like(weights_tensor)
+    )
+    weights_tensor[assistant_mask_tensor] /= weights_tensor[
+        assistant_mask_tensor
+    ].mean()
+
     return {
         "tokens": torch.tensor(pad(token_ids, pad_token_id)),
         "group_ids": torch.tensor(pad(group_ids, -1)),
         "parent_ids": torch.tensor(pad(parent_ids, -1)),
         "input_pos": torch.tensor(pad(input_pos, 0)),
-        "assistant_mask": torch.tensor(pad(assistant_mask, 0), dtype=torch.bool),
+        "assistant_mask": assistant_mask_tensor,
         "logprobs": torch.tensor(pad(logprobs, float("nan"))),
         "advantages": torch.tensor(pad(advantages, 0.0)),
+        "weights": weights_tensor,
     }
 
 
@@ -138,6 +152,7 @@ def packed_tensors_from_dir(**kwargs: Unpack[DiskPackedTensors]) -> PackedTensor
             "assistant_mask": torch.bool,
             "logprobs": torch.float32,
             "advantages": torch.float32,
+            "weights": torch.float32,
         }.items()
     }  # type: ignore
 