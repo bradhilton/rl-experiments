a6e28faeebac123b9d1317822176dc381c63d92e
Brad Hilton <brad.hilton.nw@gmail.com>
2025-02-21 17:20:20 -0700

    feat: Add surprise bonus to temporal clue grader

--- experiments/lib/temporal_clue.py
+++ experiments/lib/temporal_clue.py
@@ -1,4 +1,5 @@
 import json
+import math
 from openai.types.chat.chat_completion import Choice
 import re
 from typing import Iterable, TypedDict
@@ -16,10 +17,12 @@ def get_temporal_clue_puzzles() -> list[TemporalCluePuzzle]:
     return json.load(open("./data/bradhilton/temporal-clue/puzzles.json"))
 
 
-def get_temporal_clue_tasks() -> Iterable[Task]:
+def get_temporal_clue_tasks(surprise_bonus: float = 0.0) -> Iterable[Task]:
     for puzzle in get_temporal_clue_puzzles():
 
-        def grader(choice: Choice, puzzle: TemporalCluePuzzle = puzzle) -> float:
+        def grader(
+            choice: Choice, puzzle: TemporalCluePuzzle = puzzle
+        ) -> float | tuple[float, dict[str, float]]:
             content = choice.message.content
             assert isinstance(content, str)
             num_correct = 0
@@ -28,7 +31,21 @@ def get_temporal_clue_tasks() -> Iterable[Task]:
                     match = matches[-1]
                     if match.strip().lower() == value.lower():
                         num_correct += 1
-            return num_correct / len(puzzle["solution"])
+            reward = num_correct / len(puzzle["solution"])
+            if surprise_bonus != 0.0 and choice.logprobs:
+                if logprobs := [
+                    token_logprob.logprob
+                    for token_logprob in choice.logprobs.content
+                    or choice.logprobs.refusal
+                    or []
+                    if token_logprob.logprob is not None
+                    and not math.isnan(token_logprob.logprob)
+                ]:
+                    surprise = -sum(logprobs) / len(logprobs)
+                    return reward + surprise_bonus * surprise, dict(
+                        acc=reward, surprise=surprise
+                    )
+            return reward
 
         yield Task(
             messages=[
