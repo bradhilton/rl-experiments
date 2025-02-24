f20e8fe8aaac6de0dfc034e4e92b56008db0d66e
Brad Hilton <brad.hilton.nw@gmail.com>
2025-02-24 08:06:55 -0700

    chore: Update env to use a user path for google cloud credentials

--- experiments/lib/__init__.py
+++ experiments/lib/__init__.py
@@ -4,6 +4,6 @@ import os
 load_dotenv()
 
 if "GOOGLE_APPLICATION_CREDENTIALS" in os.environ:
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.abspath(
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.expanduser(
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
     )
--- experiments/lib/models.py
+++ experiments/lib/models.py
@@ -5,6 +5,7 @@ from torchtune.models.qwen2_5 import (
     qwen2_5_14b_base,
     qwen2_5_14b_instruct,
     qwen2_5_32b_base,
+    qwen2_5_32b_instruct,
 )
 from torchtune.models.llama3_1 import llama3_1_8b, llama3_1_70b
 from torchtune.modules import TransformerDecoder
@@ -84,6 +85,20 @@ def distilled_qwen_14b() -> Model:
     )
 
 
+def qwen_32b() -> Model:
+    assert torch.cuda.device_count() >= 4, "Qwen-32B requires at least 4 GPUs"
+    return Model(
+        base_model="Qwen/Qwen2.5-32B-Instruct",
+        tune_model=qwen2_5_32b_instruct,
+        tune_model_type="QWEN2",
+        tune_max_batch_tokens=32768,
+        tune_optimizer="torch.optim.AdamW",
+        vllm_named_arguments={},
+        tune_fsdp_cpu_offload=True,
+        tune_num_output_chunks=2,
+    )
+
+
 def distilled_qwen_32b() -> Model:
     assert torch.cuda.device_count() >= 4, "Qwen-32B requires at least 4 GPUs"
     return Model(
@@ -94,6 +109,7 @@ def distilled_qwen_32b() -> Model:
         tune_optimizer="torch.optim.AdamW",
         vllm_named_arguments={},
         tune_fsdp_cpu_offload=True,
+        tune_num_output_chunks=2,
     )
 
 
